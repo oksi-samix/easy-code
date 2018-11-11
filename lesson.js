e6384e6170a7e57d6e9c14bc6c8d70d755347ade
Oksana Bilokin <oksanabilokin@ratiose.com>
2018-11-11 10:45:05 +0200

    push brunch to remote git

--- lesson.js
+++ lesson.js
@@ -1,5 +1,24 @@
-const weather = require('yahoo-weather');
+const http = require('http');
+const fs = require('fs');
 
-weather('kharkov')
-  .then(data => { console.log(data.description); console.log(data.item.condition);  })
-  .catch(console.log);
+const server = http.createServer(
+  (req, res) => {
+    const isHtml = req.url.includes('.html');
+    const fileName = req.url.replace('/', '');
+    if(isHtml){
+      fs.readFile(fileName, (err, data) => {
+        res.setHeader('Content-Type', 'text/html');
+        if(err) return res.end('<strong>Error</strong>');
+        res.end(data.toString())
+      })
+      return
+    }
+    fs.readFile(fileName, (err, data) => {
+      res.setHeader('Content-Type', 'text/html');
+      if(err) return res.end('<strong>Error</strong>');
+      res.end(data)
+    })
+  });
+
+server.listen(5600);
+
