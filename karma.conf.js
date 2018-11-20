b1d614564743cda98b4d80c4a6ccd1d53832f66b
sheroshtan <sheroshtan1@gmail.com>
2018-11-20 21:33:46 +0200

    lesson from 20/11

---
+++ karma.conf.js
@@ -0,0 +1,81 @@
+// Karma configuration
+// Generated on Tue Nov 20 2018 20:42:33 GMT+0200 (Финляндия (зима))
+
+const webpack = require('./config/webpack.test.config');
+
+module.exports = function (config) {
+    config.set({
+
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['mocha'],
+
+
+        // list of files / patterns to load in the browser
+        files: [
+            'src/**/*.js'
+        ],
+
+
+        // list of files / patterns to exclude
+        exclude: [],
+
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'src/**/*.js': ['webpack']
+        },
+
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['mocha'],
+
+
+        // web server port
+        port: 9876,
+
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity,
+
+        plugins: [
+            'karma-mocha',
+            'karma-phantomjs-launcher',
+            'karma-webpack',
+            'karma-mocha-reporter'
+        ],
+
+        webpack
+
+    })
+}
---
+++ src/tool.js
@@ -0,0 +1,1 @@
+export const sum = (a = 0, b = 0) => a + b;
