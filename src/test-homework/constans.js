26ec071f3c3de3212ac3072942ffeffb1e89651e
Oksana Belokon <oksano4ka-b@ukr.net>
2018-11-25 14:17:32 +0200

    done homework from 23/11

---
+++ src/test-homework/constans.js
@@ -0,0 +1,6 @@
+export const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+export const defaultProduct = 'Apple';
+export const money = '$';
+
+export const users = [{age: 15}, {age: 14}, {age: 28}, {age: 18}, {age: 45}, {age: 68}, {age: 38}, {age: 22}, {age: 14}];
