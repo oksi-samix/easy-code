c4288694545438ef33c684c2b2db82d423f68c2d
Oksana_Bilokin <oksano4ka-b@ukr.net>
2018-12-05 15:15:57 +0200

    homework from 30/11

--- src/components/main/listName/ListName.js
+++ src/components/main/listName/ListName.js
@@ -1,14 +1,15 @@
-const Name = (props) => (
-  <li>
-    {props.user.firstName}
-    {props.user.lastName}
-    {props.user.age}
-  </li>
-);
+const Name = (props) => {
+  const { user } = props;
+  return (
+    <li>{`${user.firstName}-${user.lastName}-${user.age}`}</li>);
+};
 
 
-export const ListName = (props) => (
-  <ul>
-    {props.users.map(user => <Name user={user} key={user.firstName} />)}
-  </ul>
-);
+export const ListName = (props) => {
+  const { users } = props;
+  return (
+    <ul>
+      {users.map(user => <Name user={user} key={user.firstName} />)}
+    </ul>
+  );
+};
