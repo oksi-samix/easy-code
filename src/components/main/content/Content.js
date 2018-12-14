ebdaf92704a52c875b1b0f60b1c9b0c2488db7ad
oksi-samix <oksano4ka-b@ukr.net>
2018-12-14 21:51:12 +0200

    Update Content.js

--- src/components/main/content/Content.js
+++ src/components/main/content/Content.js
@@ -1,17 +1,31 @@
 import './content.scss';
-import { Greetting } from '../greetting';
-import { Numbers } from '../numbers';
-import { ListName } from '../listName';
+import {Greetting} from '../greetting';
+import {Numbers} from '../numbers';
+import {ListName} from '../listName';
+import Form from "../Form";
 
 const users = [
-  { firstName: 'Olya', lastName: 'Voronaya', age: 23 },
-  { firstName: 'Angelina', lastName: 'Vovk', age: 48 },
-  { firstName: 'Ivan', lastName: 'Dorn', age: 33 },
+  {firstName: 'Olya', lastName: 'Voronaya', age: 23},
+  {firstName: 'Angelina', lastName: 'Vovk', age: 48},
+  {firstName: 'Ivan', lastName: 'Dorn', age: 33},
 ];
 
+const onSave = (data) => {
+  console.log(data);
+}
 export const Content = () => (
+
   <section className="content">
-    <Greetting name="Oksana" />
-    <Numbers from={2} to={7} />
-    <ListName users={users} />
+    {/*<Greetting name="Oksana" />*/}
+    {/*<Numbers from={2} to={7} />*/}
+    {/* <ListName users={users} />*/}
+    <Form
+      data={{
+        email: 'bla@bla.com',
+        name: 'bla',
+        surname: 'blabla'
+      }}
+      onSave={onSave}
+    />
   </section>);
+
