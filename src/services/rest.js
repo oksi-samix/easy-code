86515ffe1cd24b0e3cda285135c2358e57f471cb
oksi-samix <oksano4ka-b@ukr.net>
2019-01-29 15:03:22 +0200

    Update rest.js

--- src/services/rest.js
+++ src/services/rest.js
@@ -1,43 +1,47 @@
-import { store } from '../store';
-
-import { setError } from '../store/status'
-
-const baseUrl = 'http://localhost:8086/';
+const BASE_URL = 'http://localhost:8086/';
 
 const request = (url, options = {}, data) => {
+  const isUserChecking = url.includes('checkUser');
+
   const settings = {
     credentials: 'include',
-    ...options
+    ...options,
   };
+
   if (data) {
+    settings.body = JSON.stringify(data);
     settings.headers = {
       'Content-type': 'application/json; charset=utf-8'
     };
-    settings.body = JSON.stringify(data);
   }
-  const req =  fetch(`${baseUrl}${url}`, settings)
-    .then(data => data.json())
+
+ // req.catch(error => !isUserChecking && store.dispatch(setError(String(error))));
+
+  return fetch(`${BASE_URL}${url}`, settings)
+    .then(res => res.json())
     .then((data) => {
-      if(data.error){
+      // { error: 'afdasfads' }
+      if (data.error) {
         throw data.error;
       }
+
       return data;
-    })
-
-  req.catch(err => !url.includes('checkUser') &&  store.dispatch(setError(err)));
-
-  return req;
+    });
 };
 
 const rest = {
   get(url) {
-    return request(url)
+    return request(url);
   },
 
   post(url, data) {
-    return request(url, {method: 'POST'}, data)
+    return request(url, { method: 'POST' }, data);
   },
+
+  put(url, data) {
+    return request(url, { method: 'PUT' }, data);
+  }
 };
 
 
-export {rest, request}
+export { rest, request };
