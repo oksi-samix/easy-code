17343b17a6ca3ee8ad3f0248afaf535c42f9c359
Oksana_Bilokin <oksano4ka-b@ukr.net>
2019-02-11 10:31:44 +0200

    test saga

--- src/store/user/UserSaga.test.js
+++ src/store/user/UserSaga.test.js
@@ -1,6 +1,8 @@
 import {put} from 'redux-saga/effects';
 import {checkSaga, logoutSaga, loginSaga} from './sagas';
 import {setUser} from './actions';
+import {setUser} from './actions';
+import {login} from '../../services/user';
 
 const user = {
   age: 19,
@@ -9,7 +11,7 @@ const user = {
   id: 0,
   lastName: "Oksana",
 };
-jest.mock('../services/user', () => ({
+jest.mock('../../services/user', () => ({
     login: jest.fn(() => Promise.resolve({
       age: 19,
       email: "admin@a.com",
@@ -33,22 +35,22 @@ describe('userSagas', () => {
   it('should set users to store when check it', () => {
     const it = checkSaga();
     const checkedUser = it.next().value;
-    
+
     expect(checkedUser).toEqual(Promise.resolve(user));
     expect(it.next(checkedUser).value).toEqual(put(setUser(checkedUser)));
   });
-  
+
   it('should login', () => {
     const it = loginSaga({data: {email: "admin@a.com", password: "admin"}});
     const loginUser = it.next().value;
     expect(loginUser).toEqual(Promise.resolve(user));
     expect(it.next(loginUser).value).toEqual(put(setUser(loginUser)));
   });
-  
+
   it('should logout', () => {
     const it = logoutSaga();
     const logOutUser = it.next().value;
-    
+
     expect(logOutUser).toEqual(null);
     expect(it.next(logOutUser).value).toEqual(put(setUser(null)));
   });
